759341bcbef7b231ce8cdd681f0fb3d02a9823b8
aboubacar toure <aboubacartoure93@gmail.com>
2024-11-02 23:25:38 -0400

    push4
    add video published date

--- watchLater.py
+++ watchLater.py
@@ -1,7 +1,3 @@
-
-
-
-#------------------------------------------------------------------------
     
   # -*- coding: utf-8 -*-
 
@@ -18,6 +14,7 @@ import googleapiclient.errors
 from datetime import timedelta
 import isodate
 import pandas as pd
+from datetime import datetime
 
 scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
 
@@ -73,7 +70,7 @@ def get_playlist_videos(playlist_id):
             break
     
     return videos
-
+'''
 def get_video_details(video_ids):
     video_details = []
     
@@ -92,7 +89,8 @@ def get_video_details(video_ids):
         print(video_id)
         
     return video_details
-
+'''
+'''
 def main():
     #playlistId ="PLqVVAK0h-QnC_Ui2kpCtL-M9kj2HkLnO1" #watch later 2
     playlistId ='PLqVVAK0h-QnB12dhma4ahehfGlmI36sxs' #watch later copy2
@@ -114,6 +112,56 @@ def main():
     output_file = "youtube_videos_sorted.xlsx"
     df.to_excel(output_file, index=False)
     print(f"Excel file '{output_file}' created successfully.")
+'''
+def get_video_details(video_ids):
+    video_details = []
+    
+    request = youtube.videos().list(
+        part="contentDetails,snippet",
+        id=",".join(video_ids)
+    )
+    response = request.execute()
+    
+    for item in response['items']:
+        video_id = item['id']
+        title = item['snippet']['title']
+        duration = isodate.parse_duration(item['contentDetails']['duration'])
+        channel_title = item['snippet']['channelTitle']
+        
+        # Get the creation date
+        creation_date = item['snippet']['publishedAt']
+        creation_date = datetime.strptime(creation_date, "%Y-%m-%dT%H:%M:%SZ")
+        
+        # Append all details including the creation date
+        video_details.append((title, duration, channel_title, video_id, creation_date))
+        
+    return video_details
+
+
+def main():
+    # playlistId for the "Watch Later" copy playlist
+    playlistId = 'PLqVVAK0h-QnB12dhma4ahehfGlmI36sxs'
+    
+    videos = get_playlist_videos(playlistId)
+    videos_sorted = sorted(videos, key=lambda x: x[1])  # Sort by duration
+    
+    # Prepare data for Excel
+    data = {
+        "Title": [video[0] for video in videos_sorted],
+        "Duration": [str(video[1]) for video in videos_sorted],
+        "URL": [f"https://www.youtube.com/watch?v={video[3]}" for video in videos_sorted],
+        "Channel Name": [video[2] for video in videos_sorted],
+        "Video ID": [video[3] for video in videos_sorted],
+        "Creation Date": [video[4] for video in videos_sorted]  # Adding creation date
+    }
+    df = pd.DataFrame(data)
+    
+    # Save to Excel file
+    output_file = "youtube_videos_sorted.xlsx"
+    df.to_excel(output_file, index=False)
+    print(f"Excel file '{output_file}' created successfully.")
+
+
 
 
 if __name__ == "__main__":
